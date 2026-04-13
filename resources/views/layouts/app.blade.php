e85d70ba8c8a4cbebbb22fec802c3770fea06c84
Miézan Sam <miezansam2023@gmail.com>
2026-04-13 00:04:40 +0000

    feat: favicon ImmoGestion

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,6 +5,10 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>ImmoGestion - @yield('title')</title>
+
+
+    <link rel="icon" type="image/svg+xml" href="{{ asset('favicon.svg') }}">
+
     <script src="https://cdn.tailwindcss.com"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
     <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
